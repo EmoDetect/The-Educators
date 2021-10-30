2ba3db1e117048acd7ea011faada64e694a5b6dc
tudormoldovann <code.moldovanx@gmail.com>
2021-10-30 14:18:02 +0300

    Google API call

--- src/pages/homepage-kids/index.js
+++ src/pages/homepage-kids/index.js
@@ -1,8 +1,13 @@
-import startup from './capture-photo.js';
+import startup from "./capture-photo.js";
+import getEmotions from "./callApi.js";
 
 let encodedImage = null;
 
-startup().then(res => {
-    encodedImage = res;
-    console.log(res);
-});
+startup().then((res) => {
+  encodedImage = res;
+  getEmotions(encodedImage).then((emotions) => {
+    console.log(emotions);
+  });
+}); 
+
+export default encodedImage;
