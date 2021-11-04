4193a8a1b0711968a9e01f2455f40268eb44f265
tudormoldovann <code.moldovanx@gmail.com>
2021-11-04 13:57:57 +0200

    Finish saving kid emotions

--- src/pages/auth/auth.js
+++
@@ -1,9 +0,0 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-import * as firebaseui from "firebaseui";
-import "firebaseui/dist/firebaseui.css";
-import "initApp";
-import uiConfig from "./authConfig";
-
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start("#firebaseui-auth-container", uiConfig);
--- src/pages/homepage-kids/gameApp.js
+++ src/pages/homepage-kids/gameApp.js
@@ -1,37 +1,35 @@
-
-const option1 = document.querySelector(".option1");
-const option2 = document.querySelector(".option2");
-const option3 = document.querySelector(".option3");
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
 let answer = 0;
 
 const generateEquation = () => {
-  let number1 = Math.trunc(Math.random() * 7) + 1;
-  let number2 = Math.trunc(Math.random() * 7) + 1;
-  let answer1 = Math.trunc(Math.random() * 7) + 1;
-  let answer2 = Math.trunc(Math.random() * 7) + 1;
+    let number1 = Math.trunc(Math.random() * 7) + 1;
+    let number2 = Math.trunc(Math.random() * 7) + 1;
+    let answer1 = Math.trunc(Math.random() * 7) + 1;
+    let answer2 = Math.trunc(Math.random() * 7) + 1;
 
-  let allAnswers = [];
-  let switchAnswers = [];
+    let allAnswers = [];
+    let switchAnswers = [];
 
-  answer = number1 + number2;
-  
-  document.querySelector(".number1").innerHTML = number1;
-  document.querySelector(".number2").innerHTML = number2;
+    answer = number1 + number2;
 
-  allAnswers = [answer, answer1, answer2];
+    document.querySelector('.number1').innerHTML = number1;
+    document.querySelector('.number2').innerHTML = number2;
 
-  for (let i = allAnswers.length; i--; ) {
-    switchAnswers.push(
-      allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
-    );
-  }
+    allAnswers = [answer, answer1, answer2];
 
-  option1.innerHTML = switchAnswers[0];
-  option2.innerHTML = switchAnswers[1];
-  option3.innerHTML = switchAnswers[2];
+    for (let i = allAnswers.length; i--; ) {
+        switchAnswers.push(
+            allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
+        );
+    }
 
-  return answer;
-}
+    option1.innerHTML = switchAnswers[0];
+    option2.innerHTML = switchAnswers[1];
+    option3.innerHTML = switchAnswers[2];
 
+    return answer;
+};
 
 export default generateEquation;
