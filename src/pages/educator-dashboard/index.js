b38f1d1befb042423f995f926d8e4f51706a24a8
cosminbarzan <barzancosmin@yahoo.com>
2021-11-04 17:58:41 +0200

    Display first emotions to dashboard educators

--- src/pages/educator-dashboard/index.js
+++ src/pages/educator-dashboard/index.js
@@ -5,3 +5,29 @@ logoutButton.addEventListener('click', () => {
     firebase.auth().signOut();
     window.location.href = '/src/pages/login/login.html';
 });
+
+const allKidsEmotions = {};
+
+let data1 = document.getElementById('data1');
+let data2 = document.getElementById('data2');
+
+let joy = document.getElementById('joy');
+let anger = document.getElementById('anger');
+
+const getKids = async () => {
+    const snapshot = await firebase.firestore().collection('kidEmotions').get()
+    
+    /* 
+        array cu toate documentele din kidEmotions
+        array[i].id = id-ul
+        array[i].data() = obiectul emotions 
+    */
+    const res = snapshot.docs.map(doc => doc);
+
+    const elem = res[1];
+
+    joy.innerHTML = 'JoyLikelihood: ' + elem.data().emotion.joyLikelihood;
+    anger.innerHTML = 'AngerLikelihood: ' + elem.data().emotion.angerLikelihood;
+}
+
+getKids();
--- src/pages/homepage-kids/gameApp.js
+++ src/pages/homepage-kids/gameApp.js
@@ -1,29 +1,28 @@
 const option1 = document.querySelector('.option1');
 const option2 = document.querySelector('.option2');
 const option3 = document.querySelector('.option3');
-let answer = 0;
 
 const generateEquation = () => {
-    let number1 = Math.trunc(Math.random() * 7) + 1;
-    let number2 = Math.trunc(Math.random() * 7) + 1;
-    let answer1 = Math.trunc(Math.random() * 7) + 1;
-    let answer2 = Math.trunc(Math.random() * 7) + 1;
+    let number1 = Math.trunc(Math.random() * 9) + 1;
+    let number2 = Math.trunc(Math.random() * 9) + 1;
 
-    let allAnswers = [];
+    let answer = number1 + number2;
+
+    let allAnswers = [answer];
+
+    while (allAnswers.length < 3) {
+        const newAnswer = Math.trunc(Math.random() * 9) + 1;
+        if (!allAnswers.includes(newAnswer)) {
+            allAnswers.push(newAnswer);
+        }
+    }
+
     let switchAnswers = [];
-
-    answer = number1 + number2;
 
     document.querySelector('.number1').innerHTML = number1;
     document.querySelector('.number2').innerHTML = number2;
 
-    allAnswers = [answer, answer1, answer2];
-
-    for (let i = allAnswers.length; i--; ) {
-        switchAnswers.push(
-            allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
-        );
-    }
+    switchAnswers = allAnswers.sort(() => Math.random() - 0.5);
 
     option1.innerHTML = switchAnswers[0];
     option2.innerHTML = switchAnswers[1];
